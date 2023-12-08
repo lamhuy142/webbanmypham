4cc592c0ac0349e064ac852074d6f16ce0af958d
lamhuy142 <147523526+lamhuy142@users.noreply.github.com>
2023-12-09 01:03:48 +0700

    up

---
+++ public/inc/bottom.php
@@ -0,0 +1,95 @@
+<section class="info_section  layout_padding2-top">
+    <div class="social_container">
+        <div class="social_box">
+            <a href="">
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+            </a>
+            <a href="">
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+            </a>
+            <a href="">
+                <i class="fa fa-instagram" aria-hidden="true"></i>
+            </a>
+            <a href="">
+                <i class="fa fa-youtube" aria-hidden="true"></i>
+            </a>
+        </div>
+    </div>
+    <div class="info_container ">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-lg-3">
+                    <h6>
+                        GIỚI THIỆU
+                    </h6>
+                    <p>
+                        Cửa hàng mỹ phẩm của chúng tôi cung cấp sự đa dạng và chất lượng cao với hàng trăm sản phẩm chăm sóc cá nhân. Chúng tôi cam kết đem đến sự tiện lợi và an toàn cho trải nghiệm mua sắm của bạn. Hãy khám phá ngay! 
+                    </p>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="info_form ">
+                        <h5>
+                            Newsletter
+                        </h5>
+                        <form action="#">
+                            <input type="email" placeholder="Enter your email">
+                            <button>
+                                Subscribe
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <h6>
+                        CẦN GIÚP ĐỠ
+                    </h6>
+                    <p>
+                        Hãy để chúng tôi giúp bạn tìm kiếm những giải pháp làm đẹp tốt nhất và mang lại sự hài lòng cho bạn. Liên hệ ngay để được hỗ trợ!
+                     </p>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <h6>
+                        THÔNG TIN LIÊN HỆ 
+                    </h6>
+                    <div class="info_link-box">
+                        <a href="">
+                            <i class="fa fa-map-marker" aria-hidden="true"></i>
+                            <span> 123 Long Xuyên An Giang </span>
+                        </a>
+                        <a href="">
+                            <i class="fa fa-phone" aria-hidden="true"></i>
+                            <span> 0987654321 </span>
+                        </a>
+                        <a href="">
+                            <i class="fa fa-envelope" aria-hidden="true"></i>
+                            <span> lhshop@gmail.com</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- footer section -->
+    <footer class=" footer_section">
+        <div class="container">
+            <p>
+                &copy; <span id="displayYear"></span>
+            </p>
+        </div>
+    </footer>
+    <!-- footer section -->
+
+</section>
+
+<!-- end info section -->
+
+
+<script src="js/jquery-3.4.1.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+</script>
+<script src="js/custom.js"></script>
+
+</body>
+
+</html>
---
+++ public/inc/top.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+
+    <title>
+        Mỹ Phẩm
+    </title>
+
+    <!-- slider stylesheet -->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+
+    <!-- bootstrap core css -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet" />
+    <!-- responsive style -->
+    <link href="css/responsive.css" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="hero_area">
+        <!-- header section strats -->
+        <header class="header_section">
+            <nav class="navbar navbar-expand-lg custom_nav-container ">
+                <a class="navbar-brand" href="index.php">
+                    <span>
+                        LH SHOP
+                    </span>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class=""></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav  ">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="index.php">Trang Chủ </a> <!-- <span class="sr-only">(current)</span>-->
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="shop.php">
+                                Cửa Hàng
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="about.php">
+                                Giới Thiệu
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.php">Liên Hệ</a>
+                        </li>
+                    </ul>
+                    <div class="user_option">
+                        <a href="">
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                            <span>
+                                Đăng Nhập
+                            </span>
+                        </a>
+                        <a href="">
+                            <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+                        </a>
+                        <form class="form-inline ">
+                            <button class="btn nav_search-btn" type="submit">
+                                <i class="fa fa-search" aria-hidden="true"></i>
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <!-- end header section -->
+        <!-- slider section -->
+
+        <section class="slider_section">
+            <div class="slider_container">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="container-fluid">
+                                <div class="row">
+                                    <div class="col-md-7">
+                                        <div class="detail-box">
+                                            <h1>
+                                                Chào Mừng Đến Với <br>
+                                                LH Shop
+                                            </h1>
+                                            <h4>
+                                                Phụ nữ hiện đại, đánh son không đánh ghen.
+                                            </h4>
+                                            <a href="">
+                                                Liên Hệ
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-5 ">
+                                        <div class="img-box">
+                                            <img src="images/slider-img.png" alt="" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="carousel-item ">
+                            <div class="container-fluid">
+                                <div class="row">
+                                    <div class="col-md-7">
+                                        <div class="detail-box">
+                                            <h1>
+                                                Chào Mừng Đến Với <br>
+                                                LH Shop
+                                            </h1>
+                                            <h4>
+                                                Phụ nữ hiện đại, đánh son không đánh ghen.
+                                            </h4>
+                                            <a href="">
+                                                Liên Hệ
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-5 ">
+                                        <div class="img-box">
+                                            <img src="images/slider-img.png" alt="" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel_btn-box">
+                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                            <i class="fa fa-arrow-left" aria-hidden="true"></i>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <img src="images/line.png" alt="" />
+                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                            <i class="fa fa-arrow-right" aria-hidden="true"></i>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- end slider section -->
+    </div>
+    <!-- end hero area -->
